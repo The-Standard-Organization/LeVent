04f882fcb090d0a2a0373b35daab42d13f445a5a
Hassan Habib <hassanhabib@live.com>
2022-09-27 23:02:48 -0700

    BROKERS: Insert Event Handler

---
+++ LeVent/Brokers/Storages/IStorageBroker.cs
@@ -0,0 +1,10 @@
+﻿// -------------------------------------------------
+// Copyright (c) PiorSoft, LLC. All rights reserved.
+// -------------------------------------------------
+
+namespace LeVent.Brokers.Storages
+{
+    public partial interface IStorageBroker<T>
+    {
+    }
+}
---
+++ LeVent/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,18 @@
+﻿// -------------------------------------------------
+// Copyright (c) PiorSoft, LLC. All rights reserved.
+// -------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LeVent.Brokers.Storages
+{
+    public partial class StorageBroker<T> : IStorageBroker<T>
+    {
+        private static List<Func<T, ValueTask>> EventHandlers;
+
+        public StorageBroker() => 
+            EventHandlers = new List<Func<T, ValueTask>>();
+    }
+}
