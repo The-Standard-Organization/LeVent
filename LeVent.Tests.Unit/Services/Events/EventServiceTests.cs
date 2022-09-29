d39a0e5a32f305493e7239f220cde6bab58cd851
Hassan Habib <hassanhabib@live.com>
2022-09-28 22:39:01 -0700

    ShouldAddEventHandler -> FAIL

---
+++ LeVent.Tests.Unit/Services/Events/EventServiceTests.cs
@@ -0,0 +1,24 @@
+﻿// -------------------------------------------------
+// Copyright (c) PiorSoft, LLC. All rights reserved.
+// -------------------------------------------------
+
+using LeVent.Brokers.Storages;
+using LeVent.Services.Events;
+using Moq;
+
+namespace LeVent.Tests.Unit.Services.Events
+{
+    public partial class EventServiceTests
+    {
+        private readonly Mock<IStorageBroker<object>> storageBrokerMock;
+        private readonly IEventService<object> eventService;
+
+        public EventServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker<object>>();
+
+            this.eventService = new EventService<object>(
+                storageBroker: this.storageBrokerMock.Object);
+        }
+    }
+}
