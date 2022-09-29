3077910470649e1e80d59296770deb21ce85f067
Kailu Hu <hukailu@piorsoft.com>
2022-09-28 22:52:10 -0700

    ShouldThrowValidationExceptionOnAddIfEventHandlerIsNull -> FAIL

---
+++ LeVent/Models/Foundations/Events/Exceptions/EventValidationException.cs
@@ -0,0 +1,14 @@
+﻿// -------------------------------------------------
+// Copyright (c) PiorSoft, LLC. All rights reserved.
+// -------------------------------------------------
+
+using Xeptions;
+
+namespace LeVent.Models.Foundations.Events.Exceptions
+{
+    public class EventValidationException : Xeption
+    {
+        public EventValidationException(Xeption innerException)
+            : base("Event validation error occurred, please fix error and try again. ", innerException) { }
+    }
+}
---
+++ LeVent/Models/Foundations/Events/Exceptions/NullEventHandlerException.cs
@@ -0,0 +1,14 @@
+﻿// -------------------------------------------------
+// Copyright (c) PiorSoft, LLC. All rights reserved.
+// -------------------------------------------------
+
+using Xeptions;
+
+namespace LeVent.Models.Foundations.Events.Exceptions
+{
+    public class NullEventHandlerException : Xeption
+    {
+        public NullEventHandlerException()
+            : base("Event handler is null") { }
+    }
+}
