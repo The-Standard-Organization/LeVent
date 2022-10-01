15995e98537f8a208dd6320f5168505d2e06c0d3
Hassan Habib <hassanhabib@live.com>
2022-10-01 01:04:20 -0700

    CLINETS: Register/Publish Events

---
+++ LeVent/Clients/LeventClient.cs
@@ -0,0 +1,24 @@
+﻿// -------------------------------------------------
+// Copyright (c) PiorSoft, LLC. All rights reserved.
+// -------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using LeVent.Services.Processings.Events;
+
+namespace LeVent.Clients
+{
+    public class LeventClient<T> : ILeventClient<T>
+    {
+        private readonly IEventProcessingService<T> eventProcessingService;
+
+        public LeventClient(IEventProcessingService<T> eventProcessingService) =>
+            this.eventProcessingService = eventProcessingService;
+
+        public async ValueTask PublishEventAsync(T @event) =>
+            await this.eventProcessingService.PublishEventAsync(@event);
+
+        public void RegisterEventHandler(Func<T, ValueTask> eventHandler) =>
+            this.eventProcessingService.AddEventHandler(eventHandler);
+    }
+}
