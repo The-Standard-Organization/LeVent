872099cc14af5b214708b634f4d841c20a291883
Kailu Hu <hukailu@piorsoft.com>
2022-09-30 22:52:27 -0700

    ShouldThrowValidationExceptionOnAddIfEventHandlerIsNull -> FAIL

---
+++ LeVent.Tests.Unit/Services/Processings/Events/EventProcessingServiceTests.Validations.Add.cs
@@ -0,0 +1,47 @@
+﻿// -------------------------------------------------
+// Copyright (c) PiorSoft, LLC. All rights reserved.
+// -------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LeVent.Models.Processings.Events.Exceptions;
+using Moq;
+using Xunit;
+
+namespace LeVent.Tests.Unit.Services.Foundations.Events
+{
+    public partial class EventProcessingServiceTests
+    {
+        [Fact]
+        public void ShouldThrowValidationExceptionOnAddIfEventHandlerIsNull()
+        {
+            // given
+            Func<object, ValueTask> nullEventHandler = null;
+
+            var nullEventHandlerProcessingException =
+                new NullEventHandlerProcessingException();
+
+            var expectedEventProcessingValidationException =
+                new EventProcessingValidationException(
+                    nullEventHandlerProcessingException);
+
+            // when
+            Action addEventHandlerAction = () =>
+                this.eventProcessingService.AddEventHandler(nullEventHandler);
+
+            EventProcessingValidationException actualEventProcessingValidationException =
+                Assert.Throws<EventProcessingValidationException>(addEventHandlerAction);
+
+            // then
+            actualEventProcessingValidationException.Should()
+                .BeEquivalentTo(expectedEventProcessingValidationException);
+
+            this.eventServiceMock.Verify(broker =>
+                broker.AddEventHandler(It.IsAny<Func<object, ValueTask>>()),
+                    Times.Never);
+
+            this.eventServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
