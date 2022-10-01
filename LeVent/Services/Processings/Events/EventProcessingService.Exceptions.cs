2fea43c8683447377589c2dd39f2ca2133d11ea5
Hassan Habib <hassanhabib@live.com>
2022-09-30 22:55:49 -0700

    ShouldThrowValidationExceptionOnAddIfEventHandlerIsNull -> PASS

---
+++ LeVent/Services/Processings/Events/EventProcessingService.Exceptions.cs
@@ -0,0 +1,25 @@
+﻿// -------------------------------------------------
+// Copyright (c) PiorSoft, LLC. All rights reserved.
+// -------------------------------------------------
+
+using LeVent.Models.Processings.Events.Exceptions;
+
+namespace LeVent.Services.Processings.Events
+{
+    public partial class EventProcessingService<T> : IEventProcessingService<T>
+    {
+        private delegate void ReturningNothingFunction();
+
+        private void TryCatch(ReturningNothingFunction returningNothingFunction)
+        {
+            try
+            {
+                returningNothingFunction();
+            }
+            catch (NullEventHandlerProcessingException nullEventHandlerProcessingException)
+            {
+                throw new EventProcessingValidationException(nullEventHandlerProcessingException);
+            }
+        }
+    }
+}
--- LeVent/Services/Processings/Events/EventProcessingService.cs
+++ LeVent/Services/Processings/Events/EventProcessingService.cs
@@ -3,24 +3,24 @@
 // -------------------------------------------------
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using LeVent.Services.Foundations.Events;
 
 namespace LeVent.Services.Processings.Events
 {
-    public partial class EventProcessingService<T> : IEventProcessingService<T> 
+    public partial class EventProcessingService<T> : IEventProcessingService<T>
     {
         private readonly IEventService<T> eventService;
 
         public EventProcessingService(IEventService<T> eventService) =>
             this.eventService = eventService;
 
-        public void AddEventHandler(Func<T, ValueTask> eventHandler)
+        public void AddEventHandler(Func<T, ValueTask> eventHandler) => 
+        TryCatch(() =>
         {
+            ValidateEventHandler(eventHandler);
+
             this.eventService.AddEventHandler(eventHandler);
-        }
+        });
     }
 }
