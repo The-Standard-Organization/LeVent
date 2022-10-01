95135799184142ca5253943e92de2066f8a76777
Hassan Habib <hassanhabib@live.com>
2022-10-01 00:29:44 -0700

    ShouldThrowValidationExceptionOnAddIfEventIsNullAsync -> FAIL

---
+++ LeVent/Models/Processings/Events/Exceptions/NullEventProcessingException.cs
@@ -0,0 +1,14 @@
+﻿// -------------------------------------------------
+// Copyright (c) PiorSoft, LLC. All rights reserved.
+// -------------------------------------------------
+
+using Xeptions;
+
+namespace LeVent.Models.Processings.Events.Exceptions
+{
+    public class NullEventProcessingException : Xeption
+    {
+        public NullEventProcessingException()
+            : base("Event is null") { }
+    }
+}
