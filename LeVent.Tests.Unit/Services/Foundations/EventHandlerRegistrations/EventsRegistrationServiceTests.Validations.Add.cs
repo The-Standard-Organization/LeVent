f16fdb79b40825ed86b55950b8262d9650b2ac6e
Hassan Habib <hassanhabib@live.com>
2022-10-08 12:17:20 -0700

    ShouldThrowValidationExceptionOnAddIfEventHandlerIsNull -> FAIL

--- LeVent.Tests.Unit/Services/Foundations/EventHandlerRegistrations/EventsRegistrationServiceTests.Validations.Add.cs
+++ LeVent.Tests.Unit/Services/Foundations/EventHandlerRegistrations/EventsRegistrationServiceTests.Validations.Add.cs
@@ -45,5 +45,50 @@ namespace LeVent.Tests.Unit.Services.Foundations.EventHandlerRegistrations
 
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public void ShouldThrowValidationExceptionOnAddIfEventHandlerIsNull()
+        {
+            // given
+            EventHandlerRegistration<object> randomEventHandlerRegistration =
+                CreateRandomEventHandlerRegistration();
+
+            EventHandlerRegistration<object> invalidEventHandlerRegistration =
+                randomEventHandlerRegistration;
+
+            invalidEventHandlerRegistration.EventHandler = null;
+
+            var invalidEventHandlerRegistrationException =
+                new InvalidEventHandlerRegistrationException();
+
+            invalidEventHandlerRegistrationException.AddData(
+                key: nameof(EventHandlerRegistration<object>.EventHandler),
+                values: "Handler is required");
+
+            var expectedEventHandlerRegistrationValidationException =
+                new EventHandlerRegistrationValidationException(
+                    invalidEventHandlerRegistrationException);
+
+            // when
+            Action addEventHandlerRegistrationAction = () =>
+                this.eventHandlerRegistrationService.AddEventHandlerRegistation(
+                    invalidEventHandlerRegistration);
+
+            EventHandlerRegistrationValidationException
+                actualEventHandlerRegistrationValidationException =
+                    Assert.Throws<EventHandlerRegistrationValidationException>(
+                        addEventHandlerRegistrationAction);
+
+            // then
+            actualEventHandlerRegistrationValidationException.Should()
+                .BeEquivalentTo(expectedEventHandlerRegistrationValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertEventHandlerRegistration(
+                    It.IsAny<EventHandlerRegistration<object>>()),
+                        Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
