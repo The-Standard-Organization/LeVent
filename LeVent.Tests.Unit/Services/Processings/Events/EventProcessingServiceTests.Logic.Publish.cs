c80da3e3183d8f0835ba4105183222735bbd970f
Hassan Habib <hassanhabib@live.com>
2022-10-08 21:56:12 -0700

    ShouldPublishEventWithEventNameAsync -> FAIL

--- LeVent.Tests.Unit/Services/Processings/Events/EventProcessingServiceTests.Logic.Publish.cs
+++ LeVent.Tests.Unit/Services/Processings/Events/EventProcessingServiceTests.Logic.Publish.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using LeVent.Models.Foundations.EventHandlerRegistrations;
 using Moq;
@@ -41,6 +42,70 @@ namespace LeVent.Tests.Unit.Services.Foundations.Events
                 eventHandlerMock.Verify(eventHandler =>
                     eventHandler(inputEvent),
                         Times.Once));
+
+            this.eventHandlerRegistrationServiceMock.Verify(service =>
+                service.RetrieveAllEventHandlerRegistrations(),
+                    Times.Once);
+
+            this.eventHandlerRegistrationServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldPublishEventWithEventNameAsync()
+        {
+            // given
+            string randomEventName = GetRandomEventName();
+            string inputEventName = randomEventName;
+            
+            List<Mock<Func<object, ValueTask>>> randomCalledEventHandlerMocks =
+                CreateRandomEventHandlerMocks();
+
+            List<Mock<Func<object, ValueTask>>> randomNonCalledEventHandlerMocks =
+                CreateRandomEventHandlerMocks();
+
+            List<EventHandlerRegistration<object>> randomTargetEventHandlerRegistrations =
+                CreateEventHandlerRegistrationsFromMocks(
+                    randomCalledEventHandlerMocks,
+                    inputEventName);
+
+            List<EventHandlerRegistration<object>> randomNonTargetEventHandlerRegistrations =
+                CreateEventHandlerRegistrationsFromMocks(
+                    randomNonCalledEventHandlerMocks,
+                    inputEventName);
+
+            List<EventHandlerRegistration<object>> retrievedTargetEventHandlerRegistrations =
+                randomTargetEventHandlerRegistrations;
+
+            List<EventHandlerRegistration<object>> retrievedNonTargetEventHandlerRegistrations =
+                randomNonTargetEventHandlerRegistrations;
+
+            List<EventHandlerRegistration<object>> retrievedAllEventHandlerRegistrations =
+                retrievedTargetEventHandlerRegistrations.Concat(
+                    retrievedNonTargetEventHandlerRegistrations)
+                        .ToList();
+
+            this.eventHandlerRegistrationServiceMock.Setup(service =>
+                service.RetrieveAllEventHandlerRegistrations())
+                    .Returns(retrievedAllEventHandlerRegistrations);
+
+            var randomEvent = new object();
+            object inputEvent = randomEvent;
+
+            // when
+            await this.eventProcessingService.PublishEventAsync(
+                inputEvent,
+                inputEventName);
+
+            // then
+            randomCalledEventHandlerMocks.ForEach(eventHandlerMock =>
+                eventHandlerMock.Verify(eventHandler =>
+                    eventHandler(inputEvent),
+                        Times.Once));
+
+            randomNonCalledEventHandlerMocks.ForEach(eventHandlerMock =>
+                eventHandlerMock.Verify(eventHandler =>
+                    eventHandler(inputEvent),
+                        Times.Never));
 
             this.eventHandlerRegistrationServiceMock.Verify(service =>
                 service.RetrieveAllEventHandlerRegistrations(),
